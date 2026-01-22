4c1e7c992f6a6232e2166520238f99b872a79fa6
boris <adcv@mail.ru>
2026-01-22 18:46:44 +0300

    6th commit to git

---
+++ src/Ontto.java
@@ -0,0 +1,17 @@
+public class Ontto {
+
+    public static void main(String[] args){
+         class SomeClass {
+            //smthng
+            int intField;
+            long longField;
+            boolean booleanField;
+            String stringField;
+        }
+
+
+
+    }
+}
+
+
--- src/chtoeto.java
+++ src/chtoeto.java
@@ -1,30 +1,29 @@
 public class Chtoeto {
 
     public static void main(String[] args){
+
     // comm
-        int [] a1 = new int[5];
-        a1[0] = 134;
-        a1[1] = 1343;
-        a1[2] = 1344;
-        a1[3] = 1345;
-        a1[4] = 12;
-        System.out.println(a1[0]);
-        System.out.println(a1[1]);
-        System.out.println(a1[2]);
-        System.out.println(a1[3]);
-        System.out.println(a1[4]);
-        //byte x = 1;n
-        //int a = 1000;
-        //long b = 1000000;
-        //float v = 1280000;
-        //double t = 1900000;
-        //boolean c = true;
-        //String s = "some string 1";
-        //System.out.println(a);
-        //System.out.println(b);
-        //System.out.println(v);
-        //System.out.println(t);
-        //System.out.println(c);
-        //System.out.println(s);
+//int [] a1 = new int[5];
+//a1[0] = 134;
+//a1[1] = 1343;
+//a1[2] = 1344;
+//a1[3] = 1345;
+//a1[4] = 12;
+//System.out.println(a1[0]);
+//System.out.println(a1[1]);
+//System.out.println(a1[2]);
+//System.out.println(a1[3]);
+//System.out.println(a1[4]);
+//byte x = 1;n
+//int a = 1000;
+//long b = 1000000;
+//float v = 1280000;
+//double t = 1900000;
+//boolean c = true;
+//String s = "some string 1";
+//System.out.println(a);
+//System.out.println(b);
+//System.out.println(v);
+//System.out.pr
     }
 }
